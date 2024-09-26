c00583afc40a6c189ede7f99c034eceef3596686
Eugeny <goodwin.s@mail.ru>
2024-09-26 19:07:11 +0300

    create

---
+++ src/main/java/pro/sky/collectionsListsAndSets/CollectionsListsAndSetsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.collectionsListsAndSets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsListsAndSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsListsAndSetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/collectionsListsAndSets/CollectionsListsAndSetsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.collectionsListsAndSets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsListsAndSetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
